cd1bb8be22673822d6678a84edb184a59c6b9381
rkovarik <roman.kovarik@magnolia-cms.com>
2022-12-23 10:10:04 +0100

    Adjust title of the login view

--- src/main/java/org/vaadin/addons/views/login/LoginView.java
+++ src/main/java/org/vaadin/addons/views/login/LoginView.java
@@ -1,27 +1,31 @@
 package org.vaadin.addons.views.login;
 
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 @Route("login")
-@PageTitle("Basic website editor demo")
 @AnonymousAllowed
 public class LoginView extends HorizontalLayout implements BeforeEnterObserver {
 
-    private final LoginForm login = new LoginForm();
+    private final LoginForm login;
 
     public LoginView() {
         addClassName("login-view");
         setSizeFull();
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        //TODO caption
+
+        LoginI18n i18n = LoginI18n.createDefault();
+        LoginI18n.Form i18nForm = i18n.getForm();
+        i18nForm.setTitle("Basic Magnolia Editor");
+
+        login = new LoginForm(i18n);
         login.setAction("login");
         login.setForgotPasswordButtonVisible(false);
         add(login);
